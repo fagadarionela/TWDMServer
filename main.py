29eb085f19912d09acabf65127c5a50bd4ff4602
Fagadar Ionela <faga.ionela@gmail.com>
2021-05-09 17:50:16 +0300

    styled the code

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+import PIL.Image as pil
 import flask
 import os
-import PIL.Image as pil
-
-import torch
 import time
-
+import torch
 from flask import send_file
 
 app = flask.Flask(__name__)
@@ -17,23 +15,15 @@ else:
 
 @app.route('/', methods=['POST'])
 def handle_request():
-    start_time1 = time.time()
     files_ids = list(flask.request.files)
-    print("\nNumber of Received Images : ", len(files_ids), "\n")
-    for file_id in files_ids:
-        imagefile = flask.request.files[file_id]
+    imagefile = flask.request.files[files_ids[0]]
 
-        with torch.no_grad():
-            default_image = pil.open(imagefile).convert('L')
+    with torch.no_grad():
+        default_image = pil.open(imagefile).convert('L')
 
-            name_dest_im = os.path.join("assets", "{}_disp.jpeg".format("result"))
-            default_image.save(name_dest_im)
+        name_dest_im = os.path.join("assets", "{}.jpeg".format("result"))
+        default_image.save(name_dest_im)
 
-            print("TIME: ")
-            print(time.time() - start_time1)
-            return send_file("assets\\result_disp.jpeg", mimetype='image/jpeg')
-    print("\n")
-    return "Image(s) Uploaded Successfully. Come Back Soon."
-
+        return send_file("assets\\result.jpeg", mimetype='image/jpeg')
 
 app.run(host="0.0.0.0", port=5000, debug=False)
