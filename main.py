cf39699a51577d5b0c6294ff70915995da6411f1
Fagadar Ionela <faga.ionela@gmail.com>
2021-05-09 18:32:50 +0300

    styled the code

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-import PIL.Image as pil
+import PIL.Image as image
 import flask
 import os
-import time
 import torch
 from flask import send_file
 
 app = flask.Flask(__name__)
-
-if (torch.cuda.is_available()):
-    device = torch.device("cuda")
-else:
-    device = torch.device("cpu")
-
 
 @app.route('/', methods=['POST'])
 def handle_request():
@@ -19,7 +12,7 @@ def handle_request():
     imagefile = flask.request.files[files_ids[0]]
 
     with torch.no_grad():
-        default_image = pil.open(imagefile).convert('L')
+        default_image = image.open(imagefile).convert('L')
 
         name_dest_im = os.path.join("assets", "{}.jpeg".format("result"))
         default_image.save(name_dest_im)
