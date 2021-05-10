3f029fe5d72abd9c581f7a0358529bf2ff37f789
Fagadar Ionela <faga.ionela@gmail.com>
2021-05-10 15:43:17 +0300

    added comments

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
+import os
+
 import PIL.Image as image
 import flask
-import os
-import torch
 from flask import send_file
 
 app = flask.Flask(__name__)
 
+
+# map the endpoint /
 @app.route('/', methods=['POST'])
 def handle_request():
+    # the list of files sent by the client
     files_ids = list(flask.request.files)
+    # get the image sent by the client
     imagefile = flask.request.files[files_ids[0]]
 
-    with torch.no_grad():
-        default_image = image.open(imagefile).convert('L')
+    # open the image and convert it greyscale
+    default_image = image.open(imagefile).convert('L')
 
-        name_dest_im = os.path.join("assets", "{}.jpeg".format("result"))
-        default_image.save(name_dest_im)
+    # construct the path and then save the processed image
+    current_path = os.path.dirname(__file__)  # Where main.py file is located
+    assets_path = os.path.join(current_path, 'assets')  # The assets folder path
+    name_dest_im = os.path.join(assets_path, "{}.jpeg".format("result"))
+    default_image.save(name_dest_im)
 
-        return send_file("assets\\result.jpeg", mimetype='image/jpeg')
+    # send the response to the client
+    return send_file("assets\\result.jpeg", mimetype='image/jpeg')
 
+
+# run the app on your local ipv4 address on port 5000
 app.run(host="0.0.0.0", port=5000, debug=False)
